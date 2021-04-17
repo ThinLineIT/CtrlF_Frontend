4cd19a816c3a4e1cca2a1361ee58cbadfe3377a4
JSH-data <super60447@gmail.com>
2021-04-17 18:07:07 +0900

    test

---
+++ src/components/Note/SideBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-container>
+        여기는 사이드 바입니다.
+    </v-container>
+</template>
+         
+<script>
+
+
+
+export default {
+    name : SideBar,
+    date() {
+        return {
+
+        }
+    },
+}
+</script>
