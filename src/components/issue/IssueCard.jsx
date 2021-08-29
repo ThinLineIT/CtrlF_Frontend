f9cb68e0ea62b87ce77916804efeafa03b6660db
JSH-data <super60447@gmail.com>
2021-08-29 16:01:53 +0900

    SVG color Setting

--- src/components/issue/IssueCard.jsx
+++ src/components/issue/IssueCard.jsx
@@ -1,24 +1,210 @@
-import IssuePage1 from '../../../public/images/issue/issue1.svg';
-import IssuePage2 from '../../../public/images/issue/issue2.svg';
-import IssuePage3 from '../../../public/images/issue/issue3.svg';
-import IssuePage4 from '../../../public/images/issue/issue4.svg';
+import IssueCreateNote from '../../../public/images/issue/card/issue_create_note.svg';
+import IssueUpdateNote from '../../../public/images/issue/card/issue_update_note.svg';
+import IssueDeleteNote from '../../../public/images/issue/card/issue_delete_note.svg';
+import IssueCreateTopic from '../../../public/images/issue/card/issue_create_topic.svg';
+import IssueUpdateTopic from '../../../public/images/issue/card/issue_update_topic.svg';
+import IssueDeleteTopic from '../../../public/images/issue/card/issue_delete_topic.svg';
+import IssueCreatePage from '../../../public/images/issue/card/issue_create_page.svg';
+import IssueUpdatePage from '../../../public/images/issue/card/issue_update_page.svg';
+import IssueDeletePage from '../../../public/images/issue/card/issue_delete_page.svg';
+import { types } from '../../constant/issueTypes';
 import styles from '../../styles/items/issue/issue_card.module.css';
 
 const IssueCard = ({ title, length, data }) => {
-  return (
-    <div className={`${styles.card} ${styles[`len_${length % 4}`]}`}>
-      <div className={styles.card__title}>{title}</div>
-
-      {length % 4 === 0 && <IssuePage1 className="svg_small" />}
-      {length % 4 === 1 && <IssuePage2 className="svg_medium" />}
-      {length % 4 === 2 && <IssuePage3 className="svg_large" />}
-      {length % 4 === 3 && <IssuePage4 className="svg_extra" />}
-      <div className={`${styles.card_hover} ${styles[`len_${length % 4}`]}`}>
-        {data.content_request.action} NOTE
-        <button className={styles.detail_btn}>ISSUE 자세히보기</button>
-      </div>
-    </div>
-  );
+  switch (length % 4) {
+    case 0:
+      return (
+        <div className={`${styles.card} ${styles[`len_${length % 4}`]}`}>
+          <div className={styles.card__title}>{title}</div>
+          {data.content_request.type === types[0][0] &&
+            data.content_request.action === types[0][1] && (
+              <IssueCreateNote className="svg_small" />
+            )}
+          {data.content_request.type === types[1][0] &&
+            data.content_request.action === types[1][1] && (
+              <IssueUpdateNote className="svg_small" />
+            )}
+          {data.content_request.type === types[2][0] &&
+            data.content_request.action === types[2][1] && (
+              <IssueDeleteNote className="svg_small" />
+            )}
+          {data.content_request.type === types[3][0] &&
+            data.content_request.action === types[3][1] && (
+              <IssueCreateTopic className="svg_small" />
+            )}
+          {data.content_request.type === types[4][0] &&
+            data.content_request.action === types[4][1] && (
+              <IssueUpdateTopic className="svg_small" />
+            )}
+          {data.content_request.type === types[5][0] &&
+            data.content_request.action === types[5][1] && (
+              <IssueDeleteTopic className="svg_small" />
+            )}
+          {data.content_request.type === types[6][0] &&
+            data.content_request.action === types[6][1] && (
+              <IssueCreatePage className="svg_small" />
+            )}
+          {data.content_request.type === types[7][0] &&
+            data.content_request.action === types[7][1] && (
+              <IssueUpdatePage className="svg_small" />
+            )}
+          {data.content_request.type === types[8][0] &&
+            data.content_request.action === types[8][1] && (
+              <IssueDeletePage className="svg_small" />
+            )}
+          <div
+            className={`${styles.card_hover} ${styles[`len_${length % 4}`]}`}
+          >
+            {data.content_request.action} NOTE
+            <button className={styles.detail_btn}>ISSUE 자세히보기</button>
+          </div>
+        </div>
+      );
+    case 1:
+      return (
+        <div className={`${styles.card} ${styles[`len_${length % 4}`]}`}>
+          <div className={styles.card__title}>{title}</div>
+          {data.content_request.type === types[0][0] &&
+            data.content_request.action === types[0][1] && (
+              <IssueCreateNote className="svg_medium" />
+            )}
+          {data.content_request.type === types[1][0] &&
+            data.content_request.action === types[1][1] && (
+              <IssueUpdateNote className="svg_medium" />
+            )}
+          {data.content_request.type === types[2][0] &&
+            data.content_request.action === types[2][1] && (
+              <IssueDeleteNote className="svg_medium" />
+            )}
+          {data.content_request.type === types[3][0] &&
+            data.content_request.action === types[3][1] && (
+              <IssueCreateTopic className="svg_medium" />
+            )}
+          {data.content_request.type === types[4][0] &&
+            data.content_request.action === types[4][1] && (
+              <IssueUpdateTopic className="svg_medium" />
+            )}
+          {data.content_request.type === types[5][0] &&
+            data.content_request.action === types[5][1] && (
+              <IssueDeleteTopic className="svg_medium" />
+            )}
+          {data.content_request.type === types[6][0] &&
+            data.content_request.action === types[6][1] && (
+              <IssueCreatePage className="svg_medium" />
+            )}
+          {data.content_request.type === types[7][0] &&
+            data.content_request.action === types[7][1] && (
+              <IssueUpdatePage className="svg_medium" />
+            )}
+          {data.content_request.type === types[8][0] &&
+            data.content_request.action === types[8][1] && (
+              <IssueDeletePage className="svg_medium" />
+            )}
+          <div
+            className={`${styles.card_hover} ${styles[`len_${length % 4}`]}`}
+          >
+            {data.content_request.action} NOTE
+            <button className={styles.detail_btn}>ISSUE 자세히보기</button>
+          </div>
+        </div>
+      );
+    case 2:
+      return (
+        <div className={`${styles.card} ${styles[`len_${length % 4}`]}`}>
+          <div className={styles.card__title}>{title}</div>
+          {data.content_request.type === types[0][0] &&
+            data.content_request.action === types[0][1] && (
+              <IssueCreateNote className="svg_large" />
+            )}
+          {data.content_request.type === types[1][0] &&
+            data.content_request.action === types[1][1] && (
+              <IssueUpdateNote className="svg_large" />
+            )}
+          {data.content_request.type === types[2][0] &&
+            data.content_request.action === types[2][1] && (
+              <IssueDeleteNote className="svg_large" />
+            )}
+          {data.content_request.type === types[3][0] &&
+            data.content_request.action === types[3][1] && (
+              <IssueCreateTopic className="svg_large" />
+            )}
+          {data.content_request.type === types[4][0] &&
+            data.content_request.action === types[4][1] && (
+              <IssueUpdateTopic className="svg_large" />
+            )}
+          {data.content_request.type === types[5][0] &&
+            data.content_request.action === types[5][1] && (
+              <IssueDeleteTopic className="svg_large" />
+            )}
+          {data.content_request.type === types[6][0] &&
+            data.content_request.action === types[6][1] && (
+              <IssueCreatePage className="svg_large" />
+            )}
+          {data.content_request.type === types[7][0] &&
+            data.content_request.action === types[7][1] && (
+              <IssueUpdatePage className="svg_large" />
+            )}
+          {data.content_request.type === types[8][0] &&
+            data.content_request.action === types[8][1] && (
+              <IssueDeletePage className="svg_large" />
+            )}
+          <div
+            className={`${styles.card_hover} ${styles[`len_${length % 4}`]}`}
+          >
+            {data.content_request.action} NOTE
+            <button className={styles.detail_btn}>ISSUE 자세히보기</button>
+          </div>
+        </div>
+      );
+    case 3:
+      return (
+        <div className={`${styles.card} ${styles[`len_${length % 4}`]}`}>
+          <div className={styles.card__title}>{title}</div>
+          {data.content_request.type === types[0][0] &&
+            data.content_request.action === types[0][1] && (
+              <IssueCreateNote className="svg_extra" />
+            )}
+          {data.content_request.type === types[1][0] &&
+            data.content_request.action === types[1][1] && (
+              <IssueUpdateNote className="svg_extra" />
+            )}
+          {data.content_request.type === types[2][0] &&
+            data.content_request.action === types[2][1] && (
+              <IssueDeleteNote className="svg_extra" />
+            )}
+          {data.content_request.type === types[3][0] &&
+            data.content_request.action === types[3][1] && (
+              <IssueCreateTopic className="svg_extra" />
+            )}
+          {data.content_request.type === types[4][0] &&
+            data.content_request.action === types[4][1] && (
+              <IssueUpdateTopic className="svg_extra" />
+            )}
+          {data.content_request.type === types[5][0] &&
+            data.content_request.action === types[5][1] && (
+              <IssueDeleteTopic className="svg_extra" />
+            )}
+          {data.content_request.type === types[6][0] &&
+            data.content_request.action === types[6][1] && (
+              <IssueCreatePage className="svg_extra" />
+            )}
+          {data.content_request.type === types[7][0] &&
+            data.content_request.action === types[7][1] && (
+              <IssueUpdatePage className="svg_extra" />
+            )}
+          {data.content_request.type === types[8][0] &&
+            data.content_request.action === types[8][1] && (
+              <IssueDeletePage className="svg_extra" />
+            )}
+          <div
+            className={`${styles.card_hover} ${styles[`len_${length % 4}`]}`}
+          >
+            {data.content_request.action} NOTE
+            <button className={styles.detail_btn}>ISSUE 자세히보기</button>
+          </div>
+        </div>
+      );
+  }
 };
 
 export default IssueCard;
---
+++ src/constant/issueTypes.js
@@ -0,0 +1,11 @@
+export const types = [
+  ['NOTE', 'CREATE'],
+  ['NOTE', 'UPDATE'],
+  ['NOTE', 'DELETE'],
+  ['TOPIC', 'CREATE'],
+  ['TOPIC', 'UPDATE'],
+  ['TOPIC', 'DELETE'],
+  ['PAGE', 'CREATE'],
+  ['PAGE', 'UPDATE'],
+  ['PAGE', 'DELETE'],
+];
