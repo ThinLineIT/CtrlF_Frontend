d6ccd06a53533f0fed30ac4e67aba6c1aa178f20
JSH-data <super60447@gmail.com>
2021-09-02 00:30:41 +0900

    design update

--- src/components/issue/IssueTag.jsx
+++ src/components/issue/IssueTag.jsx
@@ -1,19 +1,12 @@
 import IssueTagList from './IssueTagList';
 import IssueTagInput from './IssueTagInput';
 import styles from '../../styles/items/issue/issue_tag.module.css';
-import { useRecoilValue } from 'recoil';
-import { filterList as filterListAtom } from '../../store/atom';
 
 const IssueTag = () => {
-  const filters = useRecoilValue(filterListAtom);
-  const getFilteredIssue = () => {
-    console.log(filters);
-  };
   return (
     <div className={styles.tag}>
       <IssueTagInput styles={styles} />
       <IssueTagList styles={styles} />
-      <button onClick={getFilteredIssue}>임시 버튼입니다</button>
     </div>
   );
 };
--- src/components/issue/IssueTagInput.jsx
+++ src/components/issue/IssueTagInput.jsx
@@ -1,15 +1,22 @@
 import { useRecoilValue } from 'recoil';
-import { useEffect } from 'react';
 import { filterList as filterListAtom } from '../../store/atom';
 import IssueTagBtn from '../../components/issue/IssueTagBtn';
 
 const IssueTagInput = ({ styles }) => {
   const filterTags = useRecoilValue(filterListAtom);
+  const getFilteredIssue = () => {
+    console.log(filterTags);
+  };
   return (
     <div className={styles.tag__input}>
-      {filterTags.map((v, i) => (
-        <IssueTagBtn key={i} name={v} selected={true} />
-      ))}
+      <div className={styles.tag_select}>
+        {filterTags.map((v, i) => (
+          <IssueTagBtn key={i} name={v} selected={true} />
+        ))}
+      </div>
+      <div className={styles.filter_btn} onClick={getFilteredIssue}>
+        화살표
+      </div>
     </div>
   );
 };
