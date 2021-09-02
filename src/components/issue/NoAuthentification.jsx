c39da10984df431ce7ec399dfb6f528507b35b4a
JSH-data <super60447@gmail.com>
2021-09-02 20:54:06 +0900

    backup

---
+++ src/components/issue/NoAuthentification.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from 'react';
+import styles from '../../styles/items/modal/issue_modal.module.css';
+
+export default function NoAuthentification({ setNoAuth }) {
+  const backToModal = () => {
+    setNoAuth(false);
+  };
+  return (
+    <div className={styles.background}>
+      <div className={styles.noauth}>
+        <div className={styles.noauth__text}>권한이 없습니다</div>
+        <button className={styles.noauth__back} onClick={backToModal}>
+          이전
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/components/items/modal/issue_modal.jsx
+++ src/components/items/modal/issue_modal.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import NoAuthentification from '../../issue/NoAuthentification';
 import DropMenu from '../menu/DropMenu';
 import { getUserId } from '../../../utils/userCheck';
 import styles from '../../../styles/items/modal/issue_modal.module.css';
 
 export default function Modal({ setModal, data }) {
+  const [noAuth, setNoAuth] = useState(false);
   const [modalMenu, setModalMenu] = useState(false);
   useEffect(() => {
     document.body.style.overflow = 'hidden';
@@ -18,11 +20,11 @@ export default function Modal({ setModal, data }) {
   };
 
   const acceptIssue = () => {
-    alert('이슈를 승인할고야');
+    setNoAuth(true);
   };
 
   const rejectIssue = () => {
-    alert('이슈를 거절할고야');
+    setNoAuth(true);
   };
 
   const showMenu = () => {
@@ -32,60 +34,66 @@ export default function Modal({ setModal, data }) {
   const issueCancel = () => {
     const user_id = getUserId();
     if (user_id !== data.content_request.user_id) {
-      alert('권한 없습니다');
+      setNoAuth(true);
     }
   };
 
   const issueEdit = () => {
     const user_id = getUserId();
     if (user_id !== data.content_request.user_id) {
-      alert('권한 없습니다');
+      setNoAuth(true);
     }
   };
 
   return (
-    <div className={styles.background}>
-      <div className={styles.modal}>
-        {/* <div className={styles.ellipsis} onClick={showMenu}></div>
-        {modalMenu && (
-          <DropMenu setModalMenu={setModalMenu} modalMenu={modalMenu} />
-        )} */}
-        <div className={styles.modal__title}>
-          {data.content_request.action} {data.content_request.type}
-        </div>
-        <div className={styles.modal__origin}>{data.title} 타이틀</div>
-        <div className={`${styles.modal__change} ${styles.title}`}>
-          {data.content} 콘텐츠
-        </div>
-        <div className={`${styles.modal__change} ${styles.contents}`}>
-          {data.content} 콘텐츠
-        </div>
+    <>
+      {!noAuth ? (
+        <div className={styles.background}>
+          <div className={styles.modal}>
+            {/* <div className={styles.ellipsis} onClick={showMenu}></div>
+          {modalMenu && (
+            <DropMenu setModalMenu={setModalMenu} modalMenu={modalMenu} />
+          )} */}
+            <div className={styles.modal__title}>
+              {data.content_request.action} {data.content_request.type}
+            </div>
+            <div className={styles.modal__origin}>{data.title} 타이틀</div>
+            <div className={`${styles.modal__change} ${styles.title}`}>
+              {data.content} 콘텐츠
+            </div>
+            <div className={`${styles.modal__change} ${styles.contents}`}>
+              {data.content} 콘텐츠
+            </div>
 
-        <div className={styles.btns}>
-          <button className={styles.modal__btn} onClick={issueEdit}>
-            수정
-          </button>
-          <button className={styles.modal__btn} onClick={issueCancel}>
-            요청취소
-          </button>
-          <button className={styles.modal__btn} onClick={acceptIssue}>
-            승인
-          </button>
-          <button className={styles.modal__btn} onClick={rejectIssue}>
-            미승인
-          </button>
+            <div className={styles.btns}>
+              <button className={styles.modal__btn} onClick={issueEdit}>
+                수정
+              </button>
+              <button className={styles.modal__btn} onClick={issueCancel}>
+                요청취소
+              </button>
+              <button className={styles.modal__btn} onClick={acceptIssue}>
+                승인
+              </button>
+              <button className={styles.modal__btn} onClick={rejectIssue}>
+                미승인
+              </button>
 
-          {data.content_request.type === 'PAGE' ? (
-            <button className={styles.modal__btn} onClick={closeModal}>
-              자세히 보기
-            </button>
-          ) : (
-            <button className={styles.modal__btn} onClick={closeModal}>
-              닫기
-            </button>
-          )}
+              {data.content_request.type === 'PAGE' ? (
+                <button className={styles.modal__btn} onClick={closeModal}>
+                  자세히 보기
+                </button>
+              ) : (
+                <button className={styles.modal__btn} onClick={closeModal}>
+                  닫기
+                </button>
+              )}
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      ) : (
+        <NoAuthentification setNoAuth={setNoAuth} />
+      )}
+    </>
   );
 }
