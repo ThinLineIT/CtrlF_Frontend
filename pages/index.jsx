2235634e1ae66da856a4f807264fb30dddf27b13
Hyobum Lee <alsghk9701@gmail.com>
2021-09-24 22:12:31 +0900

    resolve some dummy codes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+import { useSetRecoilState } from 'recoil';
+import { isJwtActive } from '../src/store/atom';
 import Head from 'next/head';
 import Main from '../src/components/layout/main';
 
 export default function Home() {
+  const setJwt = useSetRecoilState(isJwtActive);
+  const [cookies, setCookie, removeCookie] = useCookies(['token']);
+
+  useEffect(() => {
+    cookies.token !== undefined ? setJwt(true) : setJwt(false);
+  }, []);
+
   return (
     <React.Fragment>
       <Head>
