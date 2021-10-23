486d97bd0d5bf97aca89847591d6ec4856edaee9
JSH-data <super60447@gmail.com>
2021-10-23 16:35:20 +0900

    Page Create API 테스트 실패

--- src/utils/PageCreate.js
+++ src/utils/PageCreate.js
@@ -1,15 +1,25 @@
 import axios from 'axios';
+import Cookies from 'js-cookie';
 
-export function pageCreateApi(title, summary, content, topic_id) {
+export async function pageCreateApi(title, summary, content, topic_id) {
   const data = {
     topic_id: topic_id,
     title: title,
     content: content,
     summary: summary,
   };
-  console.log(data);
-  // axios
-  //   .post(`${process.env.PUBLIC_BASE_API}/pages/`, data)
-  //   .then((res) => console.log(res))
-  //   .catch((err) => console.log(err));
+  // console.log(data);
+  let headers = Cookies.get('token');
+  // console.log(headers);
+  const request = await axios({
+    url: `${process.env.PUBLIC_BASE_API}pages/`,
+    method: 'post',
+    headers: {
+      token: headers,
+    },
+    data: data,
+  })
+    .then((res) => res)
+    .catch((err) => err);
+  console.log(request);
 }
