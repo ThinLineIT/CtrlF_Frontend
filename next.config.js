f62b87f2cf2f73d977dcf2ef793e4dfc013f8414
createhb21 <alsghk9701@gmail.com>
2022-03-06 23:59:03 +0900

    fix svg errors, rafactoring packages & babel, next,config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
-const withImages = require('next-images');
-module.exports = withImages();
-
 module.exports = {
-  images: {
-    loader: 'imgix',
-    path: 'https://noop/',
-  },
+  reactStrictMode: false,
 };
