c64273f304dd8355f14060c2a9994849e5435d11
createhb21 <alsghk9701@gmail.com>
2022-03-14 14:07:21 +0900

    re install inline-react-svg, next-images

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+const withImages = require('next-images');
+module.exports = withImages();
+
 module.exports = {
+  images: {
+    loader: 'imgix',
+    path: 'https://noop/',
+  },
   reactStrictMode: false,
 };
